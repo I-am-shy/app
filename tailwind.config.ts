cd5884cf1ea5d9d5b5ff1f51f0ae94574b17c5e7
shyshi <shyshi@zego.im>
2025-03-17 18:23:29 +0800

    更新依赖，修复外部模块的声明错误

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,20 +8,7 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   daisyui: {
-    themes: ["light", "dark", {
-      myTheme: {
-        "primary": "#009ef9",
-        "secondary": "#e6b500",
-        "accent": "#0039ff",                 
-        "neutral": "#1c0410",                  
-        "base-100": "C1D4F8", 
-        "base-200": "C1D4F8",
-        "info": "#0066ff",
-        "success": "#7a9d00",                  
-        "warning": "#d26200",
-        "error": "#ff8183",
-      }
-    }],
+    themes: ["light", "dark"],
   },
   plugins: [
     daisyui,
